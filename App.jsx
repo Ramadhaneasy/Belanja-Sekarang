a7551fbe0926cab94ea18dce130291efe6c97e7f
Ramadhaneasy <127509500+Ramadhaneasy@users.noreply.github.com>
2024-11-08 21:41:12 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+// export default function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+//   )
+// }
+
+export default function App() {
+    return (
+        <div className="text-2xl flex justify-center bg-blue-500 min-h-screen items-center">
+            <div>
+                <button
+                    className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+                    type="submit"
+                >
+                    Belanja Sekarang
+                </button>
+            </div>
+        </div>
+    );
+}
